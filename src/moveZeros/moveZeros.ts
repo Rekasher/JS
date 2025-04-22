1a921426dc10f7f34addf10ea2d6d7b935b0971e
Andrew <andreyka.zaytsev.2017@mail.ru>
2025-04-22 09:39:33 +0300

    Rewrite simple task from JS to TS

--- src/moveZeros/moveZeros.ts
+++ src/moveZeros/moveZeros.ts
@@ -1,4 +1,4 @@
-const moveZeroes = (arr: number[]) => {
+const moveZeros = (arr: number[]) => {
 
     const arrWithZeroes = arr.filter(item => item === 0);
 
@@ -11,8 +11,8 @@ const moveZeroes = (arr: number[]) => {
 
 const arr = [1, 2, 9, 3, 4, 5, 6, 7, 8, 9, 10];
 
-console.log(moveZeroes(arr));
+console.log(moveZeros(arr));
 
 const arr2 = [1, 2, 9, 30, 0, 0, 0, 0, 10];
 
-console.log(moveZeroes(arr2));
+console.log(moveZeros(arr2));
